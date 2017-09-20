b95746ac915e18cd358960f31540f150257e5e4d
Clif Liu <clif@afero.io>
2017-09-20 16:28:36 -0700

    Add af_util_parse_key_value_pair_file
    Change-Id: I7733aa5a75c6c7cd7b9a20940faa0f60fce6d01f

--- pkg/src/af_util.c
+++ pkg/src/af_util.c
@@ -15,6 +15,7 @@
 #include <stddef.h>
 #include <stdarg.h>
 #include <sys/wait.h>
+#include <ctype.h>
 
 #include "af_log.h"
 #include "af_util.h"
@@ -153,3 +154,128 @@ size_t af_util_hex_to_buffer(uint8_t *dest, size_t dest_len, const char *source,
     }
     return needed;
 }
+
+int af_util_parse_key_value_pair_file(char *path, af_key_value_pair_t *pairs, int numPairs)
+{
+    /* check params */
+    if (path == NULL || pairs == NULL || numPairs < 0) {
+        AFLOG_ERR("parse_kvp_param:path_NULL=%d,pair_NULL=%d,numPairs=%d", path==NULL, pairs==NULL, numPairs);
+        errno = EINVAL;
+        return -1;
+    }
+
+    /* open the key value pair file */
+    FILE *f = fopen(path, "r");
+    if (f == NULL) {
+        AFLOG_ERR("parse_kvp_open:errno=%d", errno);
+        return -1;
+    }
+
+    int pairNum, line = 0;
+
+    char buf[AF_PARSE_MAX_KEY_SIZE + AF_PARSE_MAX_VALUE_SIZE + 4];
+    while (fgets(buf, sizeof(buf), f) != NULL) {
+        char *c = buf, *key, *value;
+        line++;
+        while(isblank(*c)) c++;  /* skip over any leading spaces */
+        if (*c == '#') {          /* skip over comments */
+            while (*c != '\n' && *c != '\0') c++; /* skip until the end of the line or file */
+            if (*c == '\0') break;
+            continue;
+        }
+        if (*c == '\n') continue; /* blank line */
+        if (*c == '\0') break;    /* empty file */
+
+        key = c;
+
+        /* make sure the key starts right */
+        if (!isalpha(*c) && *c != '_') {
+            AFLOG_ERR("parse_kvp_bad_key_start:line=%d:key must start with an alphabetic character or '_'", line);
+            while (*c != '\n' && *c != '\0') c++; /* skip until the end of the line or file */
+            if (*c == '\0') break;
+            continue;
+        }
+        c++;
+
+        /* make sure key contains the right characters */
+        while (isalnum(*c) || *c == '_') c++;
+        if (*c != '=' && !isblank(*c)) {
+            AFLOG_ERR("parse_kvp_bad_key_body:line=%d:key must contain only alphanumeric characters and '_'", line);
+            while (*c != '\n' && *c != '\0') c++; /* skip until the end of the line or file */
+            if (*c == '\0') break;
+            continue;
+        }
+
+        /* at this point c points to the first character past the key */
+        /* find the key */
+        char last = *c;
+        *c = '\0';
+        int i;
+        for (i = 0; i < numPairs; i++) {
+            if (!strncmp(key, pairs[i].key, sizeof(pairs[i].key))) {
+                pairNum = i;
+                break;
+            }
+        }
+        if (i >= numPairs) {
+            AFLOG_WARNING("parse_kvp_key_not_found:line=%d,key=%s:key not found; ignoring", line, key);
+            *c = last;
+            while (*c != '\n' && *c != '\0') c++; /* skip until the end of the line or file */
+            if (*c == '\0') break;
+            continue;
+        }
+        *c = last;   /* restore the character after the key */
+
+        /* check for equals sign */
+        if (*c != '=') {
+            AFLOG_ERR("parse_kvp_expected_eq:line=%d:expected \"=\" character", line);
+            while (*c != '\n' && *c != '\0') c++; /* skip until the end of the line or file */
+            if (*c == '\0') break;
+            continue;
+        }
+        c++;
+
+        /* check for single quote */
+        if (*c != '\'') {
+            AFLOG_ERR("parse_kvp_expected_quote:line=%d:expected single quote", line);
+            while (*c != '\n' && *c != '\0') c++; /* skip until the end of the line or file */
+            if (*c == '\0') break;
+            continue;
+        }
+        c++;
+
+        value = c;
+        /* move to the closing single quote */
+        while (*c != '\'' && *c != '\n' && *c != '\0') c++;
+        if (*c != '\'') {
+            AFLOG_ERR("parse_kvp_unmatched_quotes:line=%d",line);
+            if (*c == '\0') break;
+            continue;
+        }
+
+        /* change the single quote to a '\0' to terminate the value string */
+        *c = '\0';
+        strncpy(pairs[pairNum].value, value, sizeof(pairs[pairNum].value));
+        pairs[pairNum].value[sizeof(pairs[pairNum].value) - 1] = '\0';
+        c++;
+
+        /* skip any trailing spaces */
+        while (isblank(*c)) c++;
+        if (*c != '\n' && *c != '\0') {
+            AFLOG_ERR("parse_kvp_unexpected_char_end:line=%d:unexpected character after quote", line);
+            while (*c != '\n' && *c != '\0') c++; /* skip until the end of the line or file */
+        }
+        if (*c == '\0') break;
+    }
+
+    /* print an error if one has occurred */
+    if (ferror(f)) {
+        AFLOG_ERR("parse_kvp_fgets:errno=%d", errno);
+        fclose(f);
+        return -1;
+    }
+
+    fclose(f);
+    return 0;
+}
+
--- pkg/src/af_util.h
+++ pkg/src/af_util.h
@@ -6,6 +6,10 @@
  * Copyright (c) 2017-present, Afero Inc. All rights reserved.
  *
  */
+
+#ifndef __AF_UTIL_H__
+#define __AF_UTIL_H__
+
 #include <sys/wait.h>
 
 extern int af_util_system(const char *format, ...);
@@ -14,3 +18,28 @@ extern int8_t af_util_file_exists(const char *filename);
 extern uint32_t af_util_read_file(const char *filename, char *buf, size_t  n);
 char *af_util_buffer_to_hex(char *dest, size_t dest_len, const uint8_t *source, size_t source_len);
 size_t af_util_hex_to_buffer(uint8_t *dest, size_t dest_len, const char *source, size_t source_len);
+
+#define AF_PARSE_MAX_KEY_SIZE   64
+#define AF_PARSE_MAX_VALUE_SIZE 64
+
+typedef struct {
+    char key[AF_PARSE_MAX_KEY_SIZE];
+    char value[AF_PARSE_MAX_VALUE_SIZE];
+} af_key_value_pair_t;
+
+/* This function parses a file containing key value pairs
+   The format is:
+
+# comment
+<key1>='<value1>'
+<key2>='<value2>'
+...
+
+Keys can contain the characters [A-Z] | [a-z] | [0-9] | _ but cannot start with [0 - 9]
+This file can be included in bash scripts.
+
+Returns -1 if failure. errno contains the error code.
+ */
+extern int af_util_parse_key_value_pair_file(char *path, af_key_value_pair_t *pairs, int numPairs);
+
+#endif // __AF_UTIL_H__
